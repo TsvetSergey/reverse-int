4905aacbe29bd9cda07d51e9344b4c75b2f0a265
TsvetSergey <gelectrodex@gmail.com>
2023-01-17 23:23:29 +0300

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = "";
+    const numModule = Math.abs(n);
+    const numberStr = numModule.toString();
+    for (let i = 0; i < numberStr.length; i = i + 1) {
+        result = `${numberStr[i]}${result}`;
+    }
+    return result * 1;
+};
